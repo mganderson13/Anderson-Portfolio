ef3284db38c7aa8a83338ac7d7aed05e33bfe660
Marianne Anderson <mganderson13@gmail.com>
2025-10-24 13:20:11 -0500

    navBar responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,24 @@
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+
+    function showSidebar() {
+        const sidebar = document.querySelector(".sideBarLinks");
+        sidebar.style.display = "flex";
+    }
+
+    function hideSidebar() {
+        const sidebar = document.querySelector(".sideBarLinks");
+        sidebar.style.display = "none";
+    }
+
     return (
         <nav>
         <div id="navBarTitle">
         <Link to="/" >Marianne Anderson</Link>
         </div>
-            <ul className="navBarLinks">
+        <ul className="sideBarLinks">
+                <li onClick={hideSidebar}><i class="fa-solid fa-xmark close"></i></li>
                 <li>
                     <Link to="/projects">Projects</Link>
                 </li>
@@ -19,6 +31,21 @@ const NavBar = () => {
                 <li>
                     <Link to="/resume">Resume</Link>
                 </li>
+            </ul>
+            <ul className="navBarLinks">
+                <li className="hideOnMobile">
+                    <Link to="/projects">Projects</Link>
+                </li>
+                <li className="hideOnMobile">
+                    <Link to="/about">About</Link>
+                </li>                
+                <li className="hideOnMobile">
+                    <Link to="/contact">Contact</Link>
+                </li>
+                <li className="hideOnMobile">
+                    <Link to="/resume">Resume</Link>
+                </li>
+                <li className="menuButton" onClick={showSidebar}><i class="fa-solid fa-bars"></i></li>
             </ul>
         </nav>
     )
